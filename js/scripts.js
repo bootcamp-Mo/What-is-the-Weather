de75eb6592f29cbc93e488b86773dc3ca661acaf
Monique <mo.sherred@gmail.com>
2023-03-05 19:34:14 -0800

    JS button search history weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,126 +1,161 @@
-const searchForm = document.querySelector('.searchForm')
-const btnSearch = document.querySelector('.bntSearch')
+// const inputField = document.getElementById(city)
+// const autocompleteList = document.getElementById(autocomplete-list)
 
 
 //const apiKey = process.env.apiKey
 
-// * Understand the API documentation: 
-// Once you have the API key, you need to understand how to use the API by reading the API 
-// documentation. The documentation should provide information on how to structure your API 
-// requests and what data you can expect to receive in response.
-
-// * Make an API call: 
-// With the API key and knowledge of how to use the API, you can now make 
-// API calls to retrieve weather data. In most cases, you would use JavaScript to make an 
-// AJAX request to the API endpoint.
-
-// * Parse the response: 
-// After making the API call, you will receive a response containing weather data in JSON 
-// format. You need to parse this response and extract the relevant data to display on your 
-// dashboard.
-
-// * Display the weather data: 
-// Once you have parsed the data, you can display the weather information on your dashboard 
-// using HTML and CSS.
-
-const apiKey = '4cafa176959ccc9551eb2fd2139e4ce4'
-const apiWeather = 'https://api.openweathermap.org/data/2.5/weather'
+const searchForm = document.querySelector('.searchForm');
+const btnSearch = document.querySelector('.bntSearch');
+const apiKey = '4cafa176959ccc9551eb2fd2139e4ce4';
+const apiWeather = 'https://api.openweathermap.org/data/2.5/weather';
 const city = 'CITY_NAME';
 
 
 
-/**======================
- * *         api request
+function cityWeather(city) {
+    fetch(`${apiWeather}?q=${city}&appid=${apiKey}&units=imperial`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const parsedData = JSON.parse(JSON.stringify(data));
+
+            const temperature = parsedData.main.temp;
+            const wind = parsedData.wind.speed;
+            const humidity = parsedData.main.humidity;
+            const date = new Date(parsedData.dt * 1000);
+            const cityName = parsedData.name;
+
+            localStorage.setItem(cityName, JSON.stringify({
+                temperature,
+                wind,
+                humidity,
+                date,
+                cityName
+            }));
+
+            console.log(`Temperature in ${cityName}: ${temperature}F`);
+            console.log(`Wind speed in ${cityName}: ${wind} m/s`);
+            console.log(`Humidity in ${cityName}: ${humidity}%`);
+            console.log(`Date in ${cityName}: ${date}`);
+            console.log(`${city}`);
+
+            displayWeather({
+                temperature,
+                wind,
+                humidity,
+                date,
+                cityName
+              });
+            
+            listCityHistory(cityName);
+        })
+        .catch(error => {
+            console.error(`Error: ${error}`);
+        });
+        
+}
+window.onload = function () {
+    cityWeather('Portland');
+}
+
+
+
+/**=======================
+ **              Button 
+ **        click click click
+ *  
  *========================**/
 
-function cityWeather (city) {
-    fetch(`${apiWeather}?q=${city}&appid=${apiKey}`)
-    .then(function(response) {return response.json() })
-    .then (function (data) {
-        console.log(data)
-    })
-    .catch(function () {
-    })
-}
-window.onload = function() {
-    cityWeather( 'Toronto' )
+
+ searchForm.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    const cityName = event.target.elements.cityName.value;
+    cityWeather(cityName);
+});
+
+
+/**=======================
+ *
+ **      Search History List
+ *             this sucked :[
+ *========================**/
+
+
+ let cityHistory = [];
+
+ function listCityHistory(city) {
+   // Push the new city to the cityHistory array
+   cityHistory.push(city);
+ 
+   // Store the updated city history array in localStorage
+   localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
+ 
+   // Get a reference to the city history list element
+   const cityHistoryList = document.querySelector('#cityHistoryList');
+ 
+   // Clear the existing content of the city history list
+   cityHistoryList.innerHTML = '';
+ 
+   // Loop through the cityHistory array and create a button for each city
+   for (let i = 0; i < cityHistory.length; i++) {
+     const cityButton = document.createElement('button');
+     cityButton.classList.add('city-history');
+     cityButton.textContent = cityHistory[i];
+ 
+     // Add a click event listener to the button
+     cityButton.addEventListener('click', function () {
+       // Get the city data from localStorage
+       const cityData = JSON.parse(localStorage.getItem(cityHistory[i]));
+      displayWeather(cityData);
+ 
+       // Do something with the city data, such as display it on the page
+       console.log(cityData);
+     });
+ 
+     // Append the button to the city history list
+     cityHistoryList.appendChild(cityButton);
+   }
+ }
+ 
+/**=======================
+ **      Display content
+ *  
+ *  
+ *========================**/
+
+
+ function displayWeather(cityData) {
+    const weatherList = document.createElement('ul');
+    const temperatureItem = document.createElement('li');
+    temperatureItem.textContent = `Temperature: ${cityData.temperature}F`;
+    const windItem = document.createElement('li');
+    windItem.textContent = `Wind speed: ${cityData.wind} m/s`;
+    const humidityItem = document.createElement('li');
+    humidityItem.textContent = `Humidity: ${cityData.humidity}%`;
+    const dateItem = document.createElement('li');
+    dateItem.textContent = `Date: ${cityData.date}`;
+    const cityNameItem = document.createElement('li');
+    cityNameItem.textContent = `City Name: ${cityData.cityName}`;
+  
+    weatherList.appendChild(temperatureItem);
+    weatherList.appendChild(windItem);
+    weatherList.appendChild(humidityItem);
+    weatherList.appendChild(dateItem);
+    weatherList.appendChild(cityNameItem);
+  
+    // Display the list in the HTML
+    const weatherContainer = document.querySelector('#weatherContainer');
+    weatherContainer.innerHTML = '';
+    weatherContainer.appendChild(weatherList);
   }
-console.log(cityWeather)
-
-// const dataRetrieve = new XMLHttpRequest()
-// dataRetrieve.open('GET', 'apiWeather', true) // .open explanation below
-//  dataRetrieve.onload = function getWeatherDate (city) {
-//     if (this.status == 200) {
-//         city = JSON.parse(this.dataRetrieve)
-//         localStorage.setItem('city', JSON.stringify(city))
-//     } 
-// }
-// console.log(dataRetrieve)
-
-// // Retrieve data from localStorage
-// var storedCity = localStorage.getItem('city');
-// if (storedCity) {
-//   var parsedData = JSON.parse(storedCity);
-//   // Use parsedData as needed
-// }
-// console.log(parsedData)
-// let searchHistory =  []
-// let searchInput = ''
-
-// render search history Container with loop that sets 
-//  will use let searchHistory =  []
-// when search
-
-// when we call this function we will need to use set and get
-//  function this (){
-//  searchHistoryContainer.innerHTML = '';
-
-//   // Start at end of history array and count down to show the most recent at the top.
-//     for (var i = searchHistory.length - 1; i >= 0; i--) {
-//     var btn = document.createElement('button');
-//     btn.setAttribute('type', 'button');
-//     btn.setAttribute('aria-controls', 'today forecast');
-//     btn.classList.add('history-btn', 'btn-history');
-
-//     // `data-search` allows access to city name when click handler is invoked
-//     btn.setAttribute('data-search', searchHistory[i]);
-//     btn.textContent = searchHistory[i];
-//     searchHistoryContainer.append(btn);
-//   }
-
-
-// function to update local history and display
-// setItem and getItem 
-
-
-
-
-// var searchFormEl = document.querySelector('#search-form');
-
-// function handleSearchFormSubmit(event) {
-//   event.preventDefault();
-
-//   var searchInputVal = document.querySelector('#search-input').value;
-//   var formatInputVal = document.querySelector('#format-input').value;
-
-//   if (!searchInputVal) {
-//     console.error('You need a search input value!');
-//     return;
-//   }
-
-//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-//   location.assign(queryString);
-// }
-
-// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
+displayWeather()
 /**============================================
- **               Explanation 
+ **               Explanation
  *=============================================**/
 
 //  * .onload
-// the onload function sets up a handler function to be called when the AJAX request is complete, 
-// and stores the parsed JSON data in the browser's local storage if the response has a status 
-// code of 200. If the response status code is not 200, the localStorage.setItem() method is not 
+// the onload function sets up a handler function to be called when the AJAX request is complete,
+// and stores the parsed JSON data in the browser's local storage if the response has a status
+// code of 200. If the response status code is not 200, the localStorage.setItem() method is not
 // called, and the data is not stored in local storage
